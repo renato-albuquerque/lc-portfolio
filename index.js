16df9b6619ac68d269dbf739dd0c0fd49768271a
renato albuquerque <renatomalb83@gmail.com>
2023-06-03 14:00:58 -0300

    added function js - share()

---
+++ index.js
@@ -0,0 +1,17 @@
+// variables
+
+const buttonShare = document.querySelector(".share")
+
+// functions
+
+function share(){
+	if (navigator.share !== undefined) {
+		navigator.share({
+			title: 'Portfólio - Luciano Costa',
+			text: 'Consultor Empresarial',
+			url: 'https://renato-albuquerque.github.io/lc-portfolio/'
+		})
+		.then(() => console.log('Successful share'))
+		.catch((error) => console.log('Error sharing', error));
+	}
+}
