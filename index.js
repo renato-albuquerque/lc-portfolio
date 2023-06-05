5b54f2c0b4a6d2b69774d08db9256154c236e833
renato albuquerque <renatomalb83@gmail.com>
2023-06-05 11:27:17 -0300

    completed hamburger menu feature

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 // variables
 
-
+const menuHamburger = document.querySelector(".fa-bars")
+const mobileMenuHide = document.querySelector(".mobileMenuHide")
+const mobileMenuOpen = document.querySelector(".mobileMenuOpen")
 
 // functions
+
+function openMenuHamburger() {
+    mobileMenuHide.classList.toggle("mobileMenuOpen")
+
+    if (mobileMenuHide.classList.contains("mobileMenuOpen")) {
+        menuHamburger.classList.remove("fa-bars")
+        menuHamburger.classList.add("fa-x")
+    } else {
+        menuHamburger.classList.add("fa-bars")
+        menuHamburger.classList.remove("fa-x")
+    }
+}
 
 function share(){
 	if (navigator.share !== undefined) {
